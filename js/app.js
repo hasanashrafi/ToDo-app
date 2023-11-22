ca7dd1d9ed7f066ef067571582384dfe08c28e14
hasanashrafi <yasashrafi>
2023-11-22 20:30:03 +0330

    updated

--- js/app.js
+++ js/app.js
@@ -1,28 +1,27 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-btn");
-const alertMessage = document.getElementById("alert-message")
-const todosBody = document.querySelector("tbody")
+const alertMessage = document.getElementById("alert-message");
+const todosBody = document.querySelector("tbody");
+const deleteAll = document.getElementById("delete-all");
 
 //create array of tasks => get todos from localStorage
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 console.log(todos);
 
 const saveToLs = () => {
     localStorage.setItem("todos", JSON.stringify(todos))
-}
-
+};
 //generate random id for todos
 const generateId = () => {
     return Math.round(Math.random() * Math.random() * Math.pow(10, 15)).toString();
-
-}
+};
 
 //create alert message
 const showAlert = (message, type) => {
     alertMessage.innerHTML = "";
-    const alert = document.createElement("p")
-    alert.innerText = message
+    const alert = document.createElement("p");
+    alert.innerText = message;
     alert.classList.add("alert");
     alert.classList.add(`alert-${type}`);
     alertMessage.append(alert);
@@ -31,30 +30,29 @@ const showAlert = (message, type) => {
     }, 3000);
 };
 const displayTodos = () => {
+    todosBody.innerHTML = "";
     if (!todos.length) {
         todosBody.innerHTML = "<tr><td colspan='4'>No Task Found</td></tr>"
         return
-    }
+    };
     todos.forEach(todo => {
         todosBody.innerHTML += `
         <tr>
             <td>${todo.task}</td>
-            <td>${todo.date  ||"No Date"}</td>
+            <td>${todo.date || "No Date"}</td>
             <td>${todo.completed ? "Completed" : "Pending"}</td>
             <td>
                 <button>Edit</button>
                 <button>Do</button>
                 <button>Delete</button>
             </td>
-            
         </tr>
-
-        `
+        `;
     });
 
-}
+};
 
-displayTodos()
+
 
 //add task to the table
 const addTask = () => {
@@ -69,19 +67,28 @@ const addTask = () => {
     //show success or error alert in html
     if (task) {
         todos.push(todo);
-        saveToLs()
-        displayTodos()
+        saveToLs();
+        displayTodos();
         taskInput.value = ""
         dateInput.value = ""
         console.log(todos);
-        showAlert("Todo Added Successfully", "success")
+        showAlert("Todo Added Successfully", "success");
     } else {
-        showAlert("Please Enter Todo", "error")
+        showAlert("Please Enter Todo", "error");
     }
 
 };
-
+//delete all todos from body and localStorage
+const deleteAllTasks = () => {
+    if (todos.length) {
+        todos = [];
+        saveToLs();
+        displayTodos();
+        showAlert("All Tasks Cleared Successfully", "success");
+    }
+}
 
 //eventListeners
+document.addEventListener("DOMContentLoaded", displayTodos);
 addButton.addEventListener("click", addTask);
-
+deleteAll.addEventListener("click", deleteAllTasks);
